8a372bdd085e304dd26ec62d295907ec34e9bd2a
Eduardo Rodrigues <87396076+edurodriguesn@users.noreply.github.com>
2024-06-21 21:26:29 -0300

    Update ComparadorRegex.c

--- ComparadorRegex.c
+++ ComparadorRegex.c
@@ -2,38 +2,44 @@
 #include <stdlib.h>
 #include <regex.h>
 
-int compara(char *argv[], char *linha){
-	regex_t reg; // aloca espaço para a estrutura do tipo regex_t
+int comparador(char *argv[], char *linha){
+    regex_t regex; // Estrutura para a expressão regular compilada
 
-  	// compila a expressão passada em argv[1] em caso de erro, a função retorna diferente de zero 
-	if (regcomp(&reg , argv[1], REG_EXTENDED|REG_NOSUB|REG_ICASE) != 0) {
-		fprintf(stderr,"Erro regcomp\n");
-		exit(1);
-	}
+    // compila a expressão passada em argv[1] em caso de erro, a função retorna diferente de zero 
+    if (regcomp(&regex, argv[1], REG_EXTENDED|REG_NOSUB|REG_ICASE) != 0) {
+        fprintf(stderr, "Erro ao compilar a expressão regular\n");
+        exit(1);
+    }
 
-  	/* tenta comparar a Expressao Regular compilada (&reg) com a linha do entrada.txt
-  	* se a função regexec retornar 0 casou, caso contrário não */
-	if ((regexec(&reg, linha, 0, (regmatch_t *)NULL, 0)) == 0)
-    		puts(linha);
+    /* tenta comparar a Expressao Regular compilada (&reg) com a linha do entrada.txt
+       se a função regexec retornar 0 casou, caso contrário não */
+    if (regexec(&regex, linha, 0, (regmatch_t *)NULL, 0) == 0)
+        puts(linha);
 }
+
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-		printf("Uso: compara <ExpReg> \n");
+    if (argc != 2) {
+        printf("Uso: compara <ExpReg> \n");
+        return 1;
+    }
 
-        FILE *arq= fopen("entrada.txt", "r");
-        size_t len= 100; // valor arbitrário
-        char *linha = malloc(len);
+    FILE *arquivo = fopen("entrada.txt", "r");
+    size_t tamanho_linha = 100; // Tamanho inicial da linha
+    char *linha = malloc(tamanho_linha);
 
-	if (!arq) {
-    		perror("entrada.txt");
-    		exit(1);
-	}
-  	while (getline(&linha, &len, arq) > 0)
-    		compara(argv,linha);
-  	if (linha)
-    		free(linha);
-        fclose(arq);
-        return 0;
+    if (!arquivo) {
+        perror("entrada.txt");
+        exit(1);
+    }
+
+    while (getline(&linha, &tamanho_linha, arquivo) > 0)
+        compara(argv, linha);
+
+    if (linha)
+        free(linha);
+
+    fclose(arquivo);
+    return 0;
 }
